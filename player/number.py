827fd422ed1ac29f5911a4778be8da5901928f37
Stelian DRAGNE <stelian.dragne@yahoo.com>
2024-02-06 13:37:43 +0200

    Verificare numar in numere extrase fara a folosi operatorul 'in'

--- player/number.py
+++ player/number.py
@@ -6,7 +6,16 @@ print(numere)
 numar_jucat = int(input("Introduceti un numar : "))
 print(numar_jucat)
 
-if numar_jucat in numere:
+# if numar_jucat in numere:
+#     print("Numarul jucat se afla in numerele extrase !")
+# else:
+#     print("Numarul jucat nu se afla in numerele extrase !")
+
+lista_noua = []
+for nr in numere:
+    if nr == numar_jucat:
+        lista_noua.append(nr)
+if lista_noua:
     print("Numarul jucat se afla in numerele extrase !")
 else:
     print("Numarul jucat nu se afla in numerele extrase !")
