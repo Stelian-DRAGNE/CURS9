460589814145659eb88e72ee8d3c3e088b8006d0
Stelian DRAGNE <stelian.dragne@yahoo.com>
2024-02-06 13:18:35 +0200

    Verifica daca un numar se afla in lista de numere jucate

--- player/number.py
+++ player/number.py
@@ -2,3 +2,11 @@ import random
 
 numere = [random.randint(1, 49) for _ in range(6)]
 print(numere)
+
+numar_jucat = int(input("Introduceti un numar : "))
+print(numar_jucat)
+
+if numar_jucat in numere:
+    print("Numarul jucat se afla in numerele extrase !")
+else:
+    print("Numarul jucat nu se afla in numerele extrase !")
