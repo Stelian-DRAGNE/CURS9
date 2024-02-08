7c367ba352250f1411c1a13d86464e27b74179c6
Stelian DRAGNE <stelian.dragne@yahoo.com>
2024-02-08 13:15:29 +0200

    Verifica daca numerele sunt in intervalul 1 -> 49

--- player/number.py
+++ player/number.py
@@ -1,7 +1,7 @@
 import random
 
 numere = [random.randint(1, 49) for _ in range(6)]
-numere_jucate = [int(input("Introduceti un numar :\n ")) for i in range(6)]
+numere_jucate = [int(input("Introduceti un numar :\n")) for i in range(6)]
 numere_comune = [nr for nr in numere_jucate if nr in numere]
 
 print(numere_jucate)
@@ -9,3 +9,13 @@ print(numere)
 print(numere_comune)
 
 print("Categoria : ", 7 - len(numere_comune) if len(numere_comune) >= 3 else 0)
+
+# for nr in numere:
+#     if nr not in range(1, 50):
+#         print(f" {nr} nu se afla in range.")
+#         break
+# else:
+#     print("Toate numerele se afla intre 1 si 49")
+
+print(all([nr in range(1, 50) for nr in numere]))
+print(all([nr in range(1, 50) for nr in numere_jucate]))
---
+++ player/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+for nr in numere:
+#     if nr not in range(1, 50):
+#         print(f" {nr} nu se afla in range.")
+#         break
+# else:
+#     print("Toate numerele se afla intre 1 si 49")
