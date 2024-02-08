3696bf3025f55142a631c3098c03b6d9d6702396
Stelian DRAGNE <stelian.dragne@yahoo.com>
2024-02-08 12:11:34 +0200

    Returneaza o lista de numere ghicite, la 6 numere extrase

--- player/number.py
+++ player/number.py
@@ -1,16 +1,25 @@
 import random
 
 numere = [random.randint(1, 49) for _ in range(6)]
+
+numere_jucate = []
+for i in range(6):
+    numar_jucat = int(input("Introduceti un numar : "))
+    numere_jucate.append(numar_jucat)
+print(numere_jucate)
 print(numere)
 
-numar_jucat = int(input("Introduceti un numar : "))
-print(numar_jucat)
+numere_comune = []
+for nr in numere_jucate:
+    if nr in numere:
+        numere_comune.append(nr)
+print(numere_comune)
 
-for nr in numere:
-    if numar_jucat == nr:
-        print("Numarul jucat se afla in numerele extrase !")
-        break
-else:
-    print("Numarul jucat nu se afla in numerele extrase !")
+# for nr in numere:
+#     if numar_jucat == nr:
+#         print("Numarul jucat se afla in numerele extrase !")
+#         break
+# else:
+#     print("Numarul jucat nu se afla in numerele extrase !")
 
-print(any([numar_jucat == nr for nr in numere]))
+# print(any([numar_jucat == nr for nr in numere]))
